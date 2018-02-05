5cbd1e2311209c03542e85059f77bf3ac5c6b5d3
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-05 16:57:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  minKey = :Null
+  minVal = 9999999999
+  name_hash.each {|key, value|
+  }
 end
